f6b0e92e7aa41d1cdf7359bbb58e50a0c57b557f
wolf132132 <wolf132132@gmail.com>
2018-09-05 23:50:57 -0700

    only readfile and toString left.

---
+++ src/catalog/Book.java
@@ -0,0 +1,104 @@
+package catalog;
+
+/** Class Book represents a book in the library catalog */
+public class Book {
+
+    // FILL IN CODE: declare private instance variables:
+    // title, author, year, averageRating, isCheckedOut
+    private String author;
+    private String title;
+    private int year;
+    private double averageRating;
+    private boolean isCheckedOut = false;
+
+
+    /**
+     * Constructor of class Book
+     * @param title title of the book
+     * @param author author of the book
+     * @param year year when the book was first published
+     * @param avgRating average rating on goodreads
+     */
+    public Book(String title, String author, int year,
+                double avgRating){
+        // FILL IN CODE
+        this.title = title;
+        this.author = author;
+        this.year = year;
+        averageRating = avgRating;
+    }
+
+    /**
+     * Return true if the book is currently checked out
+     * and false otherwise.
+     * @return value of boolean variable isCheckedOut
+     */
+    public boolean isCheckedOut() {
+        // FILL IN CODE
+        if (isCheckedOut){
+            return true;
+        }else{
+            return false; // remember to change it!
+        }
+    }
+
+    /**
+     * Return the title of the book
+     * @return title
+     */
+    public String getTitle() {
+        // FILL IN CODE
+
+        return title; // remember to change it
+    }
+
+    /** Return average rating of the book from goodreads
+     *
+     * @return average rating
+     */
+    public double getAverageRating() {
+        // FILL IN CODE
+
+        return 0; // remember to change it
+    }
+
+    /**
+     * Check out the book if it is available,
+     * by updating isCheckedOut variable.
+     * If the book is not available (checked out), return false
+     * @return true if was able to checkout the book, and false otherwise
+     */
+    public boolean checkoutBook() {
+        // FILL IN CODE
+        if(!isCheckedOut){
+            isCheckedOut = true;
+            return true;
+        }else{
+            isCheckedOut = false;
+            return false;
+        }
+        // remember to change it
+    }
+
+    /**
+     * Return the book if it is currently checked out, return true.
+     * If the book is not checked out (can not return), return false
+     * @return true if was able to return a book, false otherwise
+     */
+    public boolean returnBook() {
+        // FILL IN CODE
+        isCheckedOut = false;
+        return true; // remember to change it
+    }
+
+    /** Return a string with the information about the book
+     * in the following format:
+     * (title, author, year, averageRating)
+     * @return string representation of the book
+     */
+    public String toString() {
+        // FILL IN CODE
+        String str = "(" + title + ", " + author + ", " + String.valueOf(year)+ ", " + String.valueOf(averageRating) + ")";
+        return str; // // remember to change it
+    }
+}
---
+++ src/catalog/UserInterface.java
@@ -0,0 +1,83 @@
+package catalog;
+
+import java.util.Scanner;
+
+/** This class allows the user to interact with the system
+ * using keyboard input.
+ */
+public class UserInterface {
+    public final static String CHECKOUT  = "1";
+    public final static String RETURN  = "2";
+    public final static String PRINT_AVAILABLE  = "3";
+    public final static String EXIT  = "4";
+
+    /**
+     * The method interacts with the user allowing them
+     * to checkout a book, to return a book, to see a list
+     * of available books, and to exit the program.
+     * @param lc Library Catalog
+     */
+    public void mainLoop(LibraryCatalog lc) {
+        // FILL IN CODE
+        // Repeatedly prompt the user:
+        // To check out a book, enter 1
+        // To return a book, enter 2
+        // To see a list of books currently available, enter 3
+        // To exit, enter 4
+
+        // If the user enters 1, ask for the title, and if the book is available,
+        // check it out
+
+        // If the user enters 2, ask for the title, and if the book is currently
+        // checked out, return it.
+
+        // If a user enters 3, display all the books that are currently
+        // available (not checked out).
+
+        // If a user enters 4, exit the program
+        int userOption;
+        String nameOfBook;
+        Book book;
+        Scanner input = new Scanner(System.in);
+        while(true){
+            System.out.println("To check out a book, please insert 1.");
+            System.out.println("To return a book, please insert 2.");
+            System.out.println("To see which book is available, please insert 3.");
+            System.out.println("To exit from the program, insert 4.");
+            userOption = input.nextInt();
+            switch(userOption){
+                case 1:
+                    System.out.println("Please insert the title of the book: ");
+                    nameOfBook = input.nextLine();
+                    book = lc.findBook(nameOfBook);
+                    if (!book.isCheckedOut()){
+                        lc.checkoutBook(nameOfBook);
+                        System.out.println("The book has been checked out successfully");
+                    }else{
+                        System.out.println("The book is not available to be checked out.");
+                    }
+                    continue;
+
+                case 2:
+                    System.out.println("Please insert the title of the book: ");
+                    nameOfBook = input.nextLine();
+                    book = lc.findBook(nameOfBook);
+                    if (book.isCheckedOut()){
+                        lc.returnBook(nameOfBook);
+                    }
+                    System.out.println("The book has been returned");
+                    continue;
+
+                case 3:
+                    System.out.println("System is showing available books: ");
+                    System.out.println(lc.getAvailableBooks());
+                    continue;
+                case 4:
+                    System.out.println("System terminated.");
+                    break;
+            }
+        }
+
+
+    } // mainLoop
+}
